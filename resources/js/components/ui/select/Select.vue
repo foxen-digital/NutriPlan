7a41ea02bf4938b49d44523e0e6e4aa83602a1cf
mrdth <mrdth@zeventien.org>
2025-04-20 09:19:44 +0100

    Update Select component styling for improved user experience
    - Adjusted padding in the Select component to enhance visual appearance and usability.

--- resources/js/components/ui/select/Select.vue
+++ resources/js/components/ui/select/Select.vue
@@ -1,6 +1,6 @@
 <template>
   <select :value="modelValue"
-    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+    class="block w-full rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
     @change="$emit('update:modelValue', ($event.target as HTMLSelectElement).value === '' ? null : ($event.target as HTMLSelectElement).value)">
     <option v-if="allowEmpty" value="">None</option>
     <option v-for="option in options" :key="option.value" :value="option.value">
